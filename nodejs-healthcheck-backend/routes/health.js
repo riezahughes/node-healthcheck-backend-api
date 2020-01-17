7b80169972ba599a8c561d7dc748835d6168dc62
Dan <dan@ekwholesale.co.uk>
2020-01-17 17:58:44 +0000

    Added the prisma lib. Set up a basic 'add endpoint' endpoint

--- nodejs-healthcheck-backend/routes/health.js
+++ nodejs-healthcheck-backend/routes/health.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const { prisma } = require('../generated/prisma-client')
+
+var SlackWebhook = require('slack-webhook')
+var slack = new SlackWebhook(process.env.SLACK_WEBHOOK);
+
 router.get('/', function(req, res) {
   return res.send(
     {
@@ -35,12 +40,27 @@ router.get('/:uid', function(req, res) {
   )
 });
 
-router.post('/', function(req, res){
+router.post('/', async function(req, res){
 
+  let endpoint_name = req.body.name;
+
+  const result = await prisma.createEndpoint({
+    name: endpoint_name
+  })
+  res.json(result);
 });
 
 router.post('/:uid/error', function(req, res){
-  console.log("error found: "+req.endpoint);
+
+  let request = req.body;
+  console.log(request);
+
+  slack.send(":warning: Error found in: `" + request.endpoint + "`. Please check and if this continues contact CCP directly.");
+
+  return res.send({
+    "success": true
+  })
+
 })
 
 module.exports = router;
